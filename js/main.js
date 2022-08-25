427523646d889c44a478b80506725492e5f2b4f9
Your Name <you@example.com>
2022-08-25 17:43:34 +0900

    220825

--- js/main.js
+++ js/main.js
@@ -1,58 +1,28 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    window.addEventListener('scroll', () => {
-        let sct = window.scrollY;
-        console.log(sct);
-        sct > 0
-            ? document.querySelector('#Header').classList.add('on')
-            : document.querySelector('#Header').classList.remove('on');
-
-        sct > 500
-            ? document.querySelector('.to_top').classList.add('on')
-            : document.querySelector('.to_top').classList.remove('on');
-    });
-
-    const MainSlider = new Swiper('.latest_slider', {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
+    const MainSlider = new Swiper('.mySwiper', {
         slidesPerView: 3,
-        spaceBetween: 0,
+        spaceBetween: 30,
+        centeredSlides: true,
+        grabCursor: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
     })
 
-    const SCE_ELE = document.querySelectorAll('.active-event');
-    window.addEventListener('scroll', () => {
-        let sct = window.scrollY;
-        SCE_ELE.forEach(el => {
-            sct > el.offsetTop - 300
-                ? el.classList.add('on')
-                : el.classList.remove('on')
-        })
-    });
-
-
-    document.querySelector('.to_top').addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' })
-    });
-
-
-    document.querySelector('.mopen').addEventListener('click', (e) => {
-        const TG = e.currentTarget;
-        TG.classList.toggle('on');
-        document.querySelector('#Header').classList.toggle('ox')
-    });
-
-    document.querySelector('#Header').addEventListener('wheel', (e) => {
-        e.preventDefault() //이벤트 자체를 막음...
+    const DesignSlider = new Swiper('.design_slider', {
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     })
-
-
 
 
 
